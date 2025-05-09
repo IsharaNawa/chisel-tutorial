ab4bd23fa35927ccb6a445146a37303b22f5b424
IsharaNawa <yc.ishara@gmail.com>
2025-05-09 16:01:16 +1000

    completed tests of Max2

--- src/test/scala/problems/Max2Tests.scala
+++ src/test/scala/problems/Max2Tests.scala
@@ -11,11 +11,22 @@ class Max2Tests(c: Max2) extends PeekPokeTester(c) {
   for (i <- 0 until 10) {
 
     // Implement below ----------
+     val val1 = rnd.nextInt(256)
+     val val2 = rnd.nextInt(256)
 
-    poke(c.io.in0, 0)
-    poke(c.io.in1, 0)
+
+    poke(c.io.in0, val1)
+    poke(c.io.in1, val2)
     step(1)
-    expect(c.io.out, 1)
+    
+    if(val1>val2){
+      expect(c.io.out, val1)
+    }else{
+      expect(c.io.out, val2)
+    }
+
+    // shorthand version
+    // expect(c.io.out, if (val1 > val2) val1 else val2)
 
     // Implement above ----------
   }
