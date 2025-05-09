f6347e6b3455d348abe29ee46777bfad7789b203
IsharaNawa <yc.ishara@gmail.com>
2025-05-09 16:20:57 +1000

    done MaxN

--- src/main/scala/problems/MaxN.scala
+++ src/main/scala/problems/MaxN.scala
@@ -16,5 +16,7 @@ class MaxN(val n: Int, val w: Int) extends Module {
     val ins = Input(Vec(n, UInt(w.W)))
     val out = Output(UInt(w.W))
   })
+
+  // reduce the vector using max2, then the highest will be left
   io.out := io.ins.reduceLeft(Max2)
 }
--- src/test/scala/problems/MaxNTests.scala
+++ src/test/scala/problems/MaxNTests.scala
@@ -7,17 +7,24 @@ import chisel3.iotesters.PeekPokeTester
 //
 // Implement test for MaxN using PeekPokeTester
 //
+
+
+
 class MaxNTests(c: MaxN) extends PeekPokeTester(c) {
+
   for (i <- 0 until 10) {
     var mx = 0
     for (i <- 0 until c.n) {
       // Implement below ----------
-      poke(c.io.ins(0), 0)
+      val val1 = (1 << c.io.out.getWidth) - 1
+      poke(c.io.ins(i), val1)
+      if (val1 > mx) 
+        mx = val1
       // Implement above ----------
     }
     step(1)
     // Implement below ----------
-    expect(c.io.out, 1)
+    expect(c.io.out, mx)
     // Implement above ----------
   }
 }
