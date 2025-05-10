3e49230d0f7b807e90b89b8473d39aa1203f10f1
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2025-05-11 03:05:36 +1000

    done vecshiftregister

--- src/main/scala/problems/SingleEvenFilter.scala
+++ src/main/scala/problems/SingleEvenFilter.scala
@@ -13,36 +13,85 @@ import chisel3.util._
 //
 // EvenFilter   - indicates that input is even number
 //
+
+// this is an abstract filter class, which needs to be implemented using extending
+// it tells that T can be an any chisel data type
+// it accepts dtype parameter which is of any T chisel data type
+// this parameter can set the datatype of the inputs and outputs
 abstract class Filter[T <: Data](dtype: T) extends Module {
+
+  // io pins have valid signals in addition to actual data signals(given by bits signal)
   val io = IO(new Bundle {
     val in = Input(Valid(dtype))
     val out = Output(Valid(dtype))
   })
 }
 
+// create a class extending the filter abstract class
+// this is of type of any chisel data type
+// has 2 params, dtype is any chisel data type, f is a function which converts T to Bool
+// f seems like a function to test 
+  // 1. If the filter is a single filter(gets T and returns a boolean to indicate if its a single value)
+  // 2. If the filter is a even filter(gets T and returns a boolean to indicate if its a even value)
 class PredicateFilter[T <: Data](dtype: T, f: T => Bool) extends Filter(dtype) {
+
+  // set the valid bit of the output signal if input is valid and result returned by f is true
   io.out.valid := io.in.valid && f(io.in.bits)
+
+  // connect the output , directly to the input
   io.out.bits  := io.in.bits
 }
 
+// object for single filter
 object SingleFilter {
+
+  // this is the apply function
+  // it takes UInt type and parameter dtype
   def apply[T <: UInt](dtype: T) = 
     // Change function argument of Predicate filter below ----------
-    Module(new PredicateFilter(dtype, (x: T) => false.B))
+
+    // create a new PredicateFilter instance
+    // first parameter is dtype which can be directly passed
+    // second parameter is a function to tell if the current bits(x) is a single value
+    Module(new PredicateFilter(dtype, (x: T) => x <= 9.U))
     // Change function argument of Predicate filter above ----------
 }
 
+// object for even filter
 object EvenFilter {
+
+  // this is the apply function
+  // it takes UInt type and parameter dtype
   def apply[T <: UInt](dtype: T) = 
     // Change function argument of Predicate filter below ----------
-    Module(new PredicateFilter(dtype, (x: T) => false.B))
+
+    // create a new PredicateFilter instance
+    // first parameter is dtype which can be directly passed
+    // second parameter is a function to tell if the current bits(x) is an even value
+    Module(new PredicateFilter(dtype, (x: T) => x(0)===0.U.asBool))
     // Change function argument of Predicate filter above ----------
 }
 
+// this class again extends the abstract filter class
+// i.e. we already have required io ports here
 class SingleEvenFilter[T <: UInt](dtype: T) extends Filter(dtype) {
   // Implement composition below ----------
 
-  io.out <> io.in
+  // create a single filter
+  val single = SingleFilter(dtype)
+
+  // create an even filter
+  val even = EvenFilter(dtype)
+
+  // set the valid bit of the output signal if input is valid and result returned by f is true
+  //single.io.in.valid := io.in.valid
+  //single.io.in.bits := io.in.bits
+  // shorthand version of above two lines
+  single.io.in := io.in
+
+  // using the similar shorthand version
+  even.io.in := single.io.out
+  io.out := even.io.out
 
   // Implement composition above ----------
 }
--- src/main/scala/problems/VecShiftRegister.scala
+++ src/main/scala/problems/VecShiftRegister.scala
@@ -11,15 +11,58 @@ import chisel3._
 // Whole state should be replaced with 'ins' when loaded
 //
 class VecShiftRegister extends Module {
+
+  // create io
   val io = IO(new Bundle {
+
+    // inputs
     val ins   = Input(Vec(4, UInt(4.W)))
+
+    // enable when you need to get data parallelly
     val load  = Input(Bool())
+
+    // enable when you need to get data serially
     val shift = Input(Bool())
+
+    // output of the last register
     val out   = Output(UInt(4.W))
   })
+
   // Implement below ----------
 
-  io.out := 0.U
+  // create 4 registers
+  // each register can hold an unsigned integer
 
+  // create a vector of registers
+  val delay = Reg(Vec(4, UInt()))
+
+  // check if load is enabled
+  when(io.load){
+
+    // if so , parallel load the data by connecting the wires of inputs directly to each register
+    delay(0) := io.ins(0)
+    delay(1) := io.ins(1)
+    delay(2) := io.ins(2)
+    delay(3) := io.ins(3)
+
+  }.elsewhen(io.shift){
+
+    // if the shifting is enabled
+
+    // do the shifting by connecting registers appropriately
+    delay(0) := io.ins(0)
+    delay(1) := delay(0)
+    delay(2) := delay(1)
+    delay(3) := delay(2)
+  }
+
+  // output is the output of the last register
+  io.out := delay(3)
+
+  //IMPORTANT
+  // It's important to place the load block first. If we place shift block first and load second, 
+  // chisel will issue an error , because there can be a circular loop from the second block.
+  // If we assign first block as load, chisel already knows that there are no circular paths in the 
+  // first block, so it allows for the second block without any issues.
   // Implement above ----------
 }
